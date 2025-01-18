4c9b3a16026acf1aea6e2357f7dd6c51081a7c46
s22499 <101184602+s22499@users.noreply.github.com>
2025-01-18 11:14:36 +0100

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ def main():
     target_model=YOLO("yolov8m.yaml")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     target_model.to(device)
-    target_model.train(data="C:/dataset8/data.yaml",epochs=2000, imgsz=640, pretrained=True, optimizer="SGD", plots=True, cache='disk')
+    target_model.train(data="C:/dataset9/data.yaml",epochs=1500, imgsz=640, pretrained=True, optimizer="SGD", plots=True, cache='disk')
 
 
 if __name__ == '__main__':
